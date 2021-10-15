25098aebd09acdd791a593506c8ab0835f1790aa
Ernst Widerberg <ernstwi@kth.se>
2021-10-15 12:49:55 +0200

    Add pagination

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Pagination } from "semantic-ui-react";
 
 import ObjectComponent from "./ObjectComponent";
 import SearchForm from "./SearchForm";
@@ -12,19 +12,30 @@ class List extends React.Component {
             filter: {
                 field: "default-field",
                 value: ""
-            }
+            },
+            page: 1,
+            totalPages: 1
         };
 
         this.getData = this.getData.bind(this);
+        this.setPage = this.setPage.bind(this);
+        this.queryString = this.queryString.bind(this);
     }
 
     componentDidMount() {
         this.getData();
     }
 
+    queryString() {
+        return [
+            `limit=${process.env.PER_PAGE}`,
+            `skip=${this.state.page * process.env.PER_PAGE}`
+        ].join("&");
+    }
+
     // Fetch data from external source, update state
     getData() {
-        fetch(`${process.env.COLLECTOR_URL}/sc/v0/get`, {
+        fetch(`${process.env.COLLECTOR_URL}/sc/v0/get?${this.queryString()}`, {
             headers: {
                 Authorization: "Basic " + btoa("user1:pw1")
             }
@@ -52,6 +63,13 @@ class List extends React.Component {
         );
     }
 
+    setPage(e, { activePage: n }) {
+        this.setState({ page: n }, () => {
+            this.getData();
+            window.scrollTo(0, 0);
+        });
+    }
+
     render() {
         return (
             <div id="list-container">
@@ -71,6 +89,14 @@ class List extends React.Component {
                         console.log(data);
                         return <ObjectComponent {...data} key={data._id} />;
                     })}
+                </div>
+                <div id="pagination">
+                    <Pagination
+                        activePage={this.state.page}
+                        // totalPages={this.totalPages()}
+                        totalPages={5} // TODO
+                        onPageChange={this.setPage}
+                    />
                 </div>
             </div>
         );
