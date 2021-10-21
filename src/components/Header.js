a604959b5dc55f98578b2b2a6bfd668b37add35a
Ernst Widerberg <ernstwi@kth.se>
2021-10-21 09:16:27 +0200

    Remove leftover import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 class Header extends React.Component {
