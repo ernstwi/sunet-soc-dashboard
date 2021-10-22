e58f7f5c2c60d3178bf86b1c2048f3a7b2ee6b57
Ernst Widerberg <ernst@sunet.se>
2021-10-22 16:25:39 +0200

    Disable button ripple and elevation with a global theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { render } from "react-dom";
+
 import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import App from "./components/App";
 
+let global = createTheme({
+    components: {
+        MuiButton: {
+            defaultProps: {
+                disableRipple: true,
+                disableElevation: true
+            }
+        }
+    }
+});
+
 render(
-    <>
+    <ThemeProvider theme={global}>
         <CssBaseline />
         <App />
-    </>,
+    </ThemeProvider>,
     document.getElementById("root")
 );
