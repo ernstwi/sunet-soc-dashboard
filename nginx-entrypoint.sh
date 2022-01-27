738071e0e253790ae351729fbabad8988c5c0f76
Ernst Widerberg <ernst@sunet.se>
2022-01-27 16:24:36 +0100

    Remove whitespace

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -3,4 +3,3 @@ INJECT_FILE_SRC="${WWW_DIR}/inject_template.js"
 INJECT_FILE_DST="${WWW_DIR}/inject.js"
 envsubst < "${INJECT_FILE_SRC}" > "${INJECT_FILE_DST}"
 [ -z "$@" ] && nginx -g 'daemon off;' || $@
-
