a608586b2651a20c2eff52056a14afd19b1ba359
Ernst Widerberg <ernst@sunet.se>
2022-06-03 14:57:34 +0200

    Make functional: ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,46 +7,44 @@ import dateFormat from "dateformat";
 
 import { resultClassName } from "../util";
 
-class ListItem extends React.Component {
-    render() {
-        return (
-            <tr
-                className="list-item"
-                variant="outlined"
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    paddingTop: "1em",
-                    paddingBottom: "1em"
-                }}
-                onClick={() => (window.location = `/${this.props._id}`)}
-            >
-                <td>{dateFormat(this.props.timestamp, "isoUtcDateTime")}</td>
-                <td>
-                    {this.props.ip}:{this.props.port}
-                </td>
-                <td>{this.props.domain}</td>
-                <td>{this.props.display_name}</td>
-                <td style={{ paddingRight: 0 }}>
-                    <Card
-                        className={resultClassName(this.props.result)}
-                        variant="outlined"
-                    >
-                        {this.props.result.display_name}
-                    </Card>
-                </td>
-                <td style={{ paddingLeft: 0 }}>
-                    <Card className="reliability" variant="outlined">
-                        {this.props.result.reliability}
-                    </Card>
-                </td>
-                <td>
-                    <Button variant="contained">Re-scan</Button>
-                </td>
-            </tr>
-        );
-    }
+function ListItem(props) {
+    return (
+        <tr
+            className="list-item"
+            variant="outlined"
+            sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingTop: "1em",
+                paddingBottom: "1em"
+            }}
+            onClick={() => (window.location = `/${props._id}`)}
+        >
+            <td>{dateFormat(props.timestamp, "isoUtcDateTime")}</td>
+            <td>
+                {props.ip}:{props.port}
+            </td>
+            <td>{props.domain}</td>
+            <td>{props.display_name}</td>
+            <td style={{ paddingRight: 0 }}>
+                <Card
+                    className={resultClassName(props.result)}
+                    variant="outlined"
+                >
+                    {props.result.display_name}
+                </Card>
+            </td>
+            <td style={{ paddingLeft: 0 }}>
+                <Card className="reliability" variant="outlined">
+                    {props.result.reliability}
+                </Card>
+            </td>
+            <td>
+                <Button variant="contained">Re-scan</Button>
+            </td>
+        </tr>
+    );
 }
 
 export default ListItem;
