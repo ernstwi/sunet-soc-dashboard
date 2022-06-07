15f09c9c9c98f0fb4288a496fda3370e199734ac
Ernst Widerberg <ernst@sunet.se>
2022-06-07 16:50:32 +0200

    Make functional: Error

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,33 +5,31 @@ import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import Button from "@mui/material/Button";
 
-class Error extends React.Component {
-    static propTypes = {
-        clearError: PropTypes.func.isRequired,
-        clearToken: PropTypes.func.isRequired,
-        error: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <div id="error-container">
-                <Alert severity="error">
-                    <AlertTitle>Internal server error</AlertTitle>
-                    <p>{this.props.error}</p>
-                    <Button
-                        variant="contained"
-                        color="error"
-                        onClick={() => {
-                            this.props.clearToken();
-                            this.props.clearError();
-                        }}
-                    >
-                        Sign out
-                    </Button>
-                </Alert>
-            </div>
-        );
-    }
+function Error(props) {
+    return (
+        <div id="error-container">
+            <Alert severity="error">
+                <AlertTitle>Internal server error</AlertTitle>
+                <p>{props.error}</p>
+                <Button
+                    variant="contained"
+                    color="error"
+                    onClick={() => {
+                        props.clearToken();
+                        props.clearError();
+                    }}
+                >
+                    Sign out
+                </Button>
+            </Alert>
+        </div>
+    );
 }
+
+Error.propTypes = {
+    clearError: PropTypes.func.isRequired,
+    clearToken: PropTypes.func.isRequired,
+    error: PropTypes.string.isRequired
+};
 
 export default Error;
