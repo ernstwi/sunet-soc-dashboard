561754be8fa4d5bd55962e5ea2d918f799366430
Ernst Widerberg <ernst@sunet.se>
2022-06-07 16:19:23 +0200

    Replace env.sh with .env readable by Docker per default

--- make-inject-env.sh
+++ make-inject-env.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-source env.sh
-INJECT_FILE_SRC="inject_template.js"
-INJECT_FILE_DST="dist/inject.js"
 mkdir -p dist
-envsubst < "${INJECT_FILE_SRC}" > "${INJECT_FILE_DST}"
+INJECT_FILE_SRC=inject_template.js
+INJECT_FILE_DST=dist/inject.js
+env $(cat .env | xargs) envsubst < $INJECT_FILE_SRC > $INJECT_FILE_DST
